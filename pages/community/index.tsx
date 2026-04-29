32439137b7397031a29b6a37347fa4c450a55ff4
RobertMamur <mamurov0104@gmail.com>
2026-04-29 18:29:08 +0500

    feat : organize and test page routing system

---
+++ pages/community/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useState } from "react";
+
+const Community = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+
+  const [title, setTitle] = useState<string>("hello");
+  return (
+    <div>
+      Community{" "}
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        PressMe
+      </button>
+    </div>
+  );
+};
+
+export default Community;
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,40 +26,18 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={20}
-            priority
-          />
-          <div className={styles.intro}>
-            <h1>To get started, edit the index.tsx file.</h1>
-            <p>
-              Looking for a starting point or more instructions? Head over to{" "}
-              <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Templates
-              </a>{" "}
-              or the{" "}
-              <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learning
-              </a>{" "}
-              center.
-            </p>
-          </div>
+          <h1>PAGE ROUTER</h1>
+          <ol>
+            <li>
+              Get started by editing <code>pages/index.tsx</code>.
+            </li>
+            <li>Save and see your changes instantly.</li>
+          </ol>
+
           <div className={styles.ctas}>
             <a
               className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -67,21 +45,65 @@ export default function Home() {
                 className={styles.logo}
                 src="/vercel.svg"
                 alt="Vercel logomark"
-                width={16}
-                height={16}
+                width={20}
+                height={20}
               />
-              Deploy Now
+              Deploy now
             </a>
             <a
-              className={styles.secondary}
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
+              className={styles.secondary}
             >
-              Documentation
+              Read our docs
             </a>
           </div>
         </main>
+        <footer className={styles.footer}>
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/file.svg"
+              alt="File icon"
+              width={16}
+              height={16}
+            />
+            Learn
+          </a>
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/window.svg"
+              alt="Window icon"
+              width={16}
+              height={16}
+            />
+            Examples
+          </a>
+          <a
+            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/globe.svg"
+              alt="Globe icon"
+              width={16}
+              height={16}
+            />
+            Go to nextjs.org →
+          </a>
+        </footer>
       </div>
     </>
   );
